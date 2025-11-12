b011624c4e672f9039bd068190ffc76d98a8522e
santiago5627 <angel294hotmail.com@gmail.com>
2025-11-12 09:32:33 -0500

    LawyerController

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -78,7 +78,7 @@
         <div class="modal" id="editLawyerModal">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h2>Editar Abogado</h2>
+                    <h2>Editar Abogada</h2>
                     <button class="modal-close" id="closeEditModal">&times;</button>
                 </div>
                 <div class="modal-body">
